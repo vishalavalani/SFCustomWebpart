de745feb59edb78cc524428f130ae946e7c0f063
vishalavalani <vishal.avalani@gmail.com>
2020-02-27 16:09:06 +0530

    Add missing logger

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -2,7 +2,8 @@ import {
   Version,
   DisplayMode,
   Environment,
-  EnvironmentType
+  EnvironmentType,
+  Log
 } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
@@ -98,6 +99,15 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<
           alert("Welcome to the SharePoint Framework!");
         });
     }, 5000);
+
+    Log.info("HelloWorld", "message", this.context.serviceScope);
+    Log.warn("HelloWorld", "WARNING message", this.context.serviceScope);
+    Log.error(
+      "HelloWorld",
+      new Error("Error message"),
+      this.context.serviceScope
+    );
+    Log.verbose("HelloWorld", "VERBOSE message", this.context.serviceScope);
   }
 
   private _getMockListData(): Promise<ISPLists> {
