5493766c5d72e18945e98e7a3c92084943225328
vishalavalani <vishal.avalani@gmail.com>
2020-02-27 17:11:19 +0530

    Custom control in webpart

---
+++ src/controls/PropertyPaneContinentSelector/IPropertyPaneContinentSelectorInternalProps.ts
@@ -0,0 +1,6 @@
+import { IPropertyPaneCustomFieldProps } from "@microsoft/sp-webpart-base";
+import { IPropertyPaneContinentSelectorProps } from "./IPropertyPaneContinentSelectorProps";
+
+export interface IPropertyPaneContinentSelectorInternalProps
+  extends IPropertyPaneCustomFieldProps,
+    IPropertyPaneContinentSelectorProps {}
